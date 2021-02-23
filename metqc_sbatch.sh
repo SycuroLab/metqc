ea0d2425cdbfd0be584fbf07c90d01201b1160e4
kevmu <muirheadkevins@gmail.com>
2021-02-23 16:35:00 -0700

    Update metqc_sbatch.sh
    Changed the walltime to 7 days and added a start date timestamp.

--- metqc_sbatch.sh
+++ metqc_sbatch.sh
@@ -4,7 +4,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --time=20:00:00
+#SBATCH --time=7-00:00:00
 #SBATCH --mem=5G
 #SBATCH --error=metqc_sbatch_run.%J.err
 #SBATCH --output=metqc_sbatch_run.%J.out
@@ -12,6 +12,8 @@
 log_dir="$(pwd)"
 log_file="logs/metqc-analysis.log.txt"
 num_jobs=10
+
+echo "started at: `date`"
 
 snakemake --cluster-config cluster.json --cluster 'sbatch --partition={cluster.partition} --cpus-per-task={cluster.cpus-per-task} --nodes={cluster.nodes} --ntasks={cluster.ntasks} --time={cluster.time} --mem={cluster.mem} --output={cluster.output} --error={cluster.error}' --jobs $num_jobs --use-conda &> $log_dir/$log_file
 
