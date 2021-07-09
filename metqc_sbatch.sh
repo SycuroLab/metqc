e311ac263e2019a18cd72a1d7cd085d151e9619c
kevmu <muirheadkevins@gmail.com>
2021-07-09 13:43:51 -0600

    Update metqc_sbatch.sh
    Updated metqc_sbatch.sh on 2021-07-09. Added | grep -v "#" to the output_dir and list_files shell variables in case someone uses "#" comment lines to comment the config.yaml values. Just a fail safe so that all the files are copied over properly without any issues. Tested on command line and it works.

--- metqc_sbatch.sh
+++ metqc_sbatch.sh
@@ -17,8 +17,8 @@ echo "started at: `date`"
 
 snakemake --cluster-config cluster.json --cluster 'sbatch --partition={cluster.partition} --cpus-per-task={cluster.cpus-per-task} --nodes={cluster.nodes} --ntasks={cluster.ntasks} --time={cluster.time} --mem={cluster.mem} --output={cluster.output} --error={cluster.error}' --jobs $num_jobs --use-conda &> $log_dir/$log_file
 
-output_dir=$(grep "output_dir" < config.yaml | cut -d ' ' -f2 | sed 's/"//g')
-list_files=$(grep "list_files" < config.yaml | cut -d ' ' -f2 | sed 's/"//g')
+output_dir=$(grep "output_dir" < config.yaml | grep -v "#" | cut -d ' ' -f2 | sed 's/"//g')
+list_files=$(grep "list_files" < config.yaml | grep -v "#" | cut -d ' ' -f2 | sed 's/"//g')
 
 snakemake_file_dir="${output_dir}/snakemake_files"
 mkdir -p $snakemake_file_dir
