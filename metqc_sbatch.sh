f8ef57ab8bf10fed9e78152f3b346ad6be2640c6
Your Name <you@example.com>
2021-07-28 21:05:03 -0600

    Updated metqc_sbatch.sh on 2021-07-28.

--- metqc_sbatch.sh
+++ metqc_sbatch.sh
@@ -11,11 +11,28 @@
 
 log_dir="$(pwd)"
 log_file="logs/metqc-analysis.log.txt"
-num_jobs=50
+
+# The number of jobs for the snakemake command.
+num_jobs=60
+
+# The number of seconds to wait before checking if the output file of a snakemake rule is created.
+latency_wait=15
+
+# The number of times to restart a job if it fails.
+restart_times=10
+
+# The maximum inventory time.
+max_inventory_time=20
 
 echo "started at: `date`"
 
-snakemake --cluster-config cluster.json --cluster 'sbatch --partition={cluster.partition} --cpus-per-task={cluster.cpus-per-task} --nodes={cluster.nodes} --ntasks={cluster.ntasks} --time={cluster.time} --mem={cluster.mem} --output={cluster.output} --error={cluster.error}' --jobs $num_jobs --use-conda &> $log_dir/$log_file
+# Load the ~/.bashrc file as source.
+source ~/.bashrc
+
+# Activate the snakemake conda environment.
+conda activate snakemake
+
+snakemake --cluster-config cluster.json --cluster 'sbatch --partition={cluster.partition} --cpus-per-task={cluster.cpus-per-task} --nodes={cluster.nodes} --ntasks={cluster.ntasks} --time={cluster.time} --mem={cluster.mem} --output={cluster.output} --error={cluster.error}' --jobs $num_jobs --latency-wait $latency_wait --restart-times $restart_times --rerun-incomplete --max-inventory-time $max_inventory_time --use-conda &> $log_dir/$log_file
 
 output_dir=$(grep "output_dir" < config.yaml | grep -v "#" | cut -d ' ' -f2 | sed 's/"//g')
 list_files=$(grep "list_files" < config.yaml | grep -v "#" | cut -d ' ' -f2 | sed 's/"//g')
@@ -29,10 +46,12 @@ cp Snakefile $snakemake_file_dir
 cp config.yaml $snakemake_file_dir
 cp cluster.json $snakemake_file_dir
 cp metqc_sbatch.sh $snakemake_file_dir 
+cp metqc_run* $snakemake_file_dir
 
 cp -rf logs $snakemake_file_dir
 cp -rf utils $snakemake_file_dir
 
+python utils/scripts/parse_snakemake_command_logs.py --log_infile $log_dir/$log_file --output_dir $output_dir
 
 echo "finished with exit code $? at: `date`"
 
